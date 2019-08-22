300ff6bdb4a95fb79a25cf3c7db01bd6b13792e3
Benoit Moussaud <bmoussaud@xebialabs.com>
2019-08-22 20:39:49 +0200

    Package as Windows Service

--- backend/main.go
+++ backend/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"github.com/kardianos/service"
 	"io"
+	"log"
 	"net/http"
 	"os"
 )
@@ -19,15 +21,52 @@ func index(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	msg := fmt.Sprintf("Host: %s\nSuccessful Requests: %d", host, count)
+	msg := fmt.Sprintf("* Host: %s\n* Successful Requests: %d", host, count)
 	count += 1
 
 	io.WriteString(w, msg)
 }
 
-func main() {
+var logger service.Logger
+
+type program struct{}
+
+func (p *program) Start(s service.Service) error {
+	// Start should not block. Do the actual work async.
+	go p.run()
+	return nil
+}
+func (p *program) run() {
 	http.HandleFunc("/", index)
 	port := ":8000"
 	fmt.Printf("******* Starting to service on port %s\n", port)
 	http.ListenAndServe(port, nil)
+	// Do work here
+}
+func (p *program) Stop(s service.Service) error {
+	// Stop should not block. Return with a few seconds.
+	return nil
+}
+
+func main() {
+	fmt.Printf("******* Backend Service")
+	svcConfig := &service.Config{
+		Name:        "BackendService",
+		DisplayName: "Core Backend Service",
+		Description: "The core backend service",
+	}
+
+	prg := &program{}
+	s, err := service.New(prg, svcConfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+	logger, err = s.Logger(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = s.Run()
+	if err != nil {
+		logger.Error(err)
+	}
 }
--- build_win.sh
+++ build_win.sh
@@ -1,1 +1,4 @@
+rm bin/backend.exe
+env GOOS=windows GOARCH=386  go get -u github.com/kardianos/service
 env GOOS=windows GOARCH=386  go build -o bin/backend.exe -v backend/main.go
+xl apply -f xebialabs.yaml
