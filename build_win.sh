4c77ba6baf36e647b73bed98980f29f752c31fc5
Benoit Moussaud <bmoussaud@xebialabs.com>
2019-08-30 11:56:26 +0200

    build

--- build_win.sh
+++ build_win.sh
@@ -1,14 +1,15 @@
-rm -rf bin
-#env GOOS=windows GOARCH=386 go get -u github.com/kardianos/service
-#env GOOS=windows GOARCH=386 go get -u github.com/magiconair/properties
+cd cats
+./build_win.sh
+cd ..
 
-echo "** backend..."
-env GOOS=windows GOARCH=386  go build -o bin/backend/backend.exe -v backend/main.go
-cp -r backend/config.properties bin/backend
+cd dogs
+./build_win.sh
+cd ..
 
-echo "** frontend..."
-env GOOS=windows GOARCH=386  go build -o bin/frontend/frontend.exe -v frontend/main.go
-cp -r frontend/content bin/frontend/content
-cp -r frontend/config.properties bin/frontend
-echo "** xl..."
-xl apply -s --proceed-when-dirty -f xebialabs.yaml
+cd pets
+./build_win.sh
+cd ..
+
+cd gui
+./build_win.sh
+cd ..
--- gui/build_win.sh
+++ gui/build_win.sh
@@ -1,5 +1,5 @@
 ng build
 
 xl apply -s --proceed-when-dirty -f xebialabs.yaml
-#../xld.sh Applications/.NET/services/pets/1.0.4  Environments/Dev/micropet.dev
+../xld.sh Applications/.NET/services/gui/1.0.4  Environments/Dev/micropet.dev
 
