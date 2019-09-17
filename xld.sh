19042379105484351ae92c5f3a5ee972be41cfb9
Benoit Moussaud <bmoussaud@xebialabs.com>
2019-09-17 15:54:03 +0200

    deploy from Makefile

--- xld.sh
+++ xld.sh
@@ -11,6 +11,6 @@ spec:
 " > /tmp/deploy.yaml
 
 cat /tmp/deploy.yaml
-xl preview --xl-deploy-url http://localhost:4516 -f /tmp/deploy.yaml
+xl preview --xl-deploy-url http://localhost:4525 -f /tmp/deploy.yaml
 
-xl apply --xl-deploy-url http://localhost:4516 -f /tmp/deploy.yaml
+xl apply --xl-deploy-url http://localhost:4525 -f /tmp/deploy.yaml
