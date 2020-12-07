a44e63b320f4c4d641fe91fe8262652d2a1b47d1
Benoit Moussaud <bmoussaud@xebialabs.com>
2020-12-07 13:21:57 +0100

    make it work back after 6 months

--- gui/src/app/pets/pets.component.ts
+++ gui/src/app/pets/pets.component.ts
@@ -36,6 +36,8 @@ export class PetsComponent implements OnInit {
   }
 
   private refresh() {
+    console.log("------------------- refresh")
+    console.log(this.config.petServiceUrl)
     this.http.get(this.config.petServiceUrl)
       .pipe(map(result => result['Pets']))
       .subscribe(result => {
