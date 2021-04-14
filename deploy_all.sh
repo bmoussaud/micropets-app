5fc9be18a353228997184c11af1e033cd6801096
Benoit Moussaud <benoit@moussaud.org>
2021-04-14 15:02:37 +0200

    deploy with kapp

--- deploy_all.sh
+++ deploy_all.sh
@@ -1,5 +1,12 @@
-kubectl apply -k dogs/k8s
-kubectl apply -k cats/k8s
-kubectl apply -k fishes/k8s
-kubectl apply -k pets/k8s
-kubectl apply -k gui/k8s
+
+kustomize build dogs/k8s |  kapp -y deploy -n micropetdev -a dogs -f -
+kustomize build cats/k8s |  kapp -y deploy -n micropetdev -a cats -f -
+kustomize build fishes/k8s |  kapp -y deploy -n micropetdev -a fishes -f -
+kustomize build pets/k8s |  kapp -y deploy -n micropetdev -a pets -f -
+kustomize build gui/k8s |  kapp -y deploy -n micropetdev -a gui -f -
+
+#kubectl apply -k dogs/k8s
+#kubectl apply -k cats/k8s
+#kubectl apply -k fishes/k8s
+#kubectl apply -k pets/k8s
+#kubectl apply -k gui/k8s
--- undeploy_all.sh
+++ undeploy_all.sh
@@ -1,5 +1,12 @@
-kubectl delete -k dogs/k8s
-kubectl delete -k cats/k8s
-kubectl delete -k fishes/k8s
-kubectl delete -k pets/k8s
-kubectl delete -k gui/k8s
+#kubectl delete -k dogs/k8s
+#kubectl delete -k cats/k8s
+#kubectl delete -k fishes/k8s
+#kubectl delete -k pets/k8s
+#kubectl delete -k gui/k8s
+
+kapp -y delete -n micropetdev -a dogs
+kapp -y delete -n micropetdev -a cats
+kapp -y delete -n micropetdev -a fishes
+kapp -y delete -n micropetdev -a pets
+kapp -y delete -n micropetdev -a gui
+
