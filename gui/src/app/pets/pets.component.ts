378e2475a2cce48bc06dab3c1d00661bd084c537
Benoit Moussaud <benoit@moussaud.org>
2021-06-28 07:59:33 +0200

    add service list

--- gui/src/app/pets/pets.component.ts
+++ gui/src/app/pets/pets.component.ts
@@ -3,8 +3,9 @@ import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { Location } from '@angular/common';
 import { map } from 'rxjs/operators'
-import { MatTableDataSource} from '@angular/material';
-import { ConfigAssetLoaderService, Configuration} from '../config-asset-loader.service';
+import { MatTableDataSource } from '@angular/material';
+import { ConfigAssetLoaderService, Configuration } from '../config-asset-loader.service';
+import { ThrowStmt } from '@angular/compiler';
 
 
 @Component({
@@ -15,19 +16,22 @@ import { ConfigAssetLoaderService, Configuration} from '../config-asset-loader.s
 export class PetsComponent implements OnInit {
 
   public pets: any[] = []
+  public hostnames: any[] = []
   public env: string;
+  public hostnamesstr: string
   public env_color: string;
   public dataSource: MatTableDataSource<any>;
+  public dataSourceHostnames: MatTableDataSource<any>;
 
   public config: Configuration
 
-  displayedColumns = ['name','kind','age','pic']
+  displayedColumns = ['name', 'kind', 'age', 'pic']
 
   constructor(private http: HttpClient, private router: Router, private location: Location, private configService: ConfigAssetLoaderService) {
-    this.configService.loadConfigurations().subscribe(data =>   this.config = {
+    this.configService.loadConfigurations().subscribe(data => this.config = {
       petServiceUrl: (data as any).petServiceUrl,
-      stage:  (data as any).stage,
-      stage_color:  (data as any).stage_color,
+      stage: (data as any).stage,
+      stage_color: (data as any).stage_color,
     });
   }
 
@@ -42,12 +46,21 @@ export class PetsComponent implements OnInit {
     //console.log("------------------- refresh")
     //console.log(this.config.petServiceUrl)
     this.http.get(this.config.petServiceUrl)
-      .pipe(map(result => result['Pets']))
+      .pipe(map(result => result))
       .subscribe(result => {
-        this.pets = result;
+        this.pets = result['Pets'];
+        this.hostnames = result['Hostnames'];        
+        var h:string[] = new Array(4) 
+        for (let index = 0; index < this.hostnames.length; index++) {
+          const element = this.hostnames[index];
+          h[index] = element.Hostname
+        }
+        this.hostnamesstr = h.join(", ")
+
         this.env = this.config.stage;
-        this.env_color  = this.config.stage_color;
-        this.dataSource = new MatTableDataSource(this.pets)
+        this.env_color = this.config.stage_color;
+        this.dataSource = new MatTableDataSource(this.pets);
+        this.dataSourceHostnames = new MatTableDataSource(this.hostnames)
       });
   }
 
