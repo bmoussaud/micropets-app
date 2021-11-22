6a4787045e9ed8a236d3378fbf8457727ab3317f
Benoit Moussaud <benoit@moussaud.org>
2021-11-22 13:39:35 +0100

    Revert "upgrade"
    This reverts commit 885edf93131bd7cd30996ec5757edf5b18cf4959.

--- gui/src/app/app.module.ts
+++ gui/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PetsComponent } from './pets/pets.component';
 import { HttpClientModule} from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 import { ConfigAssetLoaderService} from './config-asset-loader.service';
 
@@ -16,6 +17,7 @@ import { ConfigAssetLoaderService} from './config-asset-loader.service';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    BrowserAnimationsModule,
     MaterialModule
   ],
   providers: [{
