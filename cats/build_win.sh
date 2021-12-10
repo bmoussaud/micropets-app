9884c51249eb97d49c9e9dbac657e567d93976d9
Benoit Moussaud <benoit@moussaud.org>
2021-12-10 11:48:26 +0100

    remove build.sh

--- cats/build_win.sh
+++
@@ -1,9 +0,0 @@
-rm cat.exe
-export XL_VALUE_version=$1
-#env GOOS=windows GOARCH=386 go get -u github.com/kardianos/service
-#env GOOS=windows GOARCH=386 go get -u github.com/magiconair/properties
-
-env GOOS=windows GOARCH=386  go build -o cat.exe -v main.go
-xl apply -s --proceed-when-dirty -f xebialabs.yaml
-../xld.sh Applications/.NET/services/cats/$XL_VALUE_version  Environments/MicroPet/Dev/micropet.dev
-
--- gui/build_win.sh
+++
@@ -1,6 +0,0 @@
-ng build
-export XL_VALUE_version=$1
-
-xl apply -s --proceed-when-dirty -f xebialabs.yaml
-../xld.sh Applications/.NET/services/gui/$XL_VALUE_version  Environments/MicroPet/Dev/micropet.dev
-
