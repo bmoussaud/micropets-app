5fbc6a49b2a330a6fec5f42e0604df6b50b5ab00
Benoit Moussaud <benoit@moussaud.org>
2022-12-15 17:56:20 +0100

    pets broker: remove the dependenciies with the app (app.kubernetes.io/part-of=micropets-app)

--- pets/internal/config.go
+++ pets/internal/config.go
@@ -134,7 +134,7 @@ func GetK8SServices(namespace string) (Config, error) {
 	clientset := kubernetes.NewForConfigOrDie(k8sconfig)
 
 	listOptions := metav1.ListOptions{
-		LabelSelector: "app.kubernetes.io/part-of=micropets-app, micropets/kind=backend",
+		LabelSelector: "micropets/kind=backend",
 		Limit:         100,
 	}
 
