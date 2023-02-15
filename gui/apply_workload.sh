61c48c51415f3643cc392311c3fd8940e852beb4
Benoit Moussaud <benoit@moussaud.org>
2023-02-15 15:47:45 +0100

    manage namespaace in gui

--- gui/apply_workload.sh
+++ gui/apply_workload.sh
@@ -1,1 +1,1 @@
-tanzu apps workload apply -f config/workload.yaml --live-update --local-path . --source-image akseutap4registry.azurecr.io/gui --namespace dev-tap --yes  --update-strategy merge
+tanzu apps workload apply -f config/workload.yaml -e "PETS_SERVICE=http://pets-golang.dev-tap/pets" --live-update --local-path . --source-image akseutap4registry.azurecr.io/gui --namespace dev-tap --yes  --update-strategy merge
