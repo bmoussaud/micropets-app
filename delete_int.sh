a3633582cfe40af0bf3d511c22d10262a20b455f
Benoit Moussaud <benoit@moussaud.org>
2023-02-15 11:22:36 +0100

    update the workload ci definition

---
+++ delete_int.sh
@@ -0,0 +1,5 @@
+kubectl delete -f cats/config -n micropets-int
+kubectl delete -f dogs/config -n micropets-int
+kubectl delete -f fishes/config -n micropets-int
+kubectl delete -f pets/config -n micropets-int
+kubectl delete -f gui/config -n micropets-int
