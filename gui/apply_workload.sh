4f3660963b73d582ae9268e24aaee99ce265add1
Benoit Moussaud <benoit@moussaud.org>
2023-04-12 14:57:01 +0200

    fix catalog location

--- gui/apply_workload.sh
+++ gui/apply_workload.sh
@@ -1,3 +1,3 @@
 NS=micropets-dev
 kubectl apply -f config/gui_config.yaml --namespace $NS
-tanzu apps workload apply -f config/workload.yaml -e "PETS_SERVICE=http://pets-golang.micropets-dev/pets" --live-update --local-path . --source-image akseutap4registry.azurecr.io/gui --namespace $NS --yes  --update-strategy merge
+tanzu apps workload apply -f config/workload.yaml -e "PETS_SERVICE=http://pets-golang.micropets-dev/pets" --live-update --local-path . --source-image akseutap5registry.azurecr.io/gui --namespace $NS --yes  --update-strategy merge
